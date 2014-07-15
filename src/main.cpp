5398b745f6317f854bcfd9d0e3c849f0f5853542
Arusekk <Arusekk@gmail.com>
2014-07-15 19:55:44 +0200

    Fixed bare skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "dungeon-xrawler.h"
 
 int main() {
+  printf("Witaj, synu.");
+#ifdef __WIN32
+  system("pause");
+#else
+  printf("\nPress any key to continue . . . ");
+  getchar();
+#endif
   return 0;
 }
