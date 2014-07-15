7068c68038382c3de7a64fdd723f8951e5abe36b
Zbyhu <ziggi17@wp.pl>
2014-07-15 21:42:46 +0200

    player module beggined with possible movement detection

---
+++ src/player.cpp
@@ -0,0 +1,39 @@
+#include "dungeon-xrawler.h"
+int allowpoz(int h, int w)
+{
+bool up, down, left, right;
+up = down = left = right = true;	
+if(board[h+1][w] == '#')
+up = false;	
+if(board[h-1][w] == '#')
+down = false;		
+if(board[h][w+1] == '#')
+right = false;		
+if(board[h][w-1] == '#')
+left = false;
+  return (up<<3)+(down<<2)+(left<<1)+right;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/player.h
@@ -0,0 +1,4 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+int allowpoz(int h, int w);
+#endif // __PLAYER_H__
