fd74594ac0f17177ef8e0e8c3ba0965162446f67
Arusekk <Arusekk@gmail.com>
2014-07-18 10:58:54 +0200

    Added --help option

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,14 @@ int main(int argc, char *argv[]) {
   
   init_ui();
   if (argc>1) {
+    if (strcmp(argc[1],"-h")==0 || strcmp(argc[1],"--help")==0) {
+      /// please leave untranslated the words -h and --help, and the paths like `levels/1.txt'
+      printf(_("Usage: `%s [<file> ...]'\n"
+	       "    or `%s -h|--help'\n"
+	       "\n"
+	       "<file> is of format `levels/1.txt' or `art/stickman.txt'"), basename(argv[0]), basename(argv[0]));
+    }
+    /// please leave `%d` as it is changed into the number
     printf(ngettext("showing %d ASCII-image\n","showing %d ASCII-images\n",argc-1), argc-1);
     for (int i=1; i<argc; i++) {
       rdboard(argv[i]);
